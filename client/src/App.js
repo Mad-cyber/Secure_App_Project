29aad9c5ae7078bb26102db714aded823c274949
Mad-cyber <msker.ryan@gmail.com>
2023-04-02 13:43:49 +0100

    change to posts from database

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,51 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Nav from './Nav';
-import axios from'axios';
+import axios from 'axios';
 
-const App = () => (
-    <div className="container pb-5">
-        <Nav />
-        <br />
-        <h1>NCI SMART LOGIN</h1>
-    </div>
-);
+const App = () => {
+    const [blogs, setBlogs] = useState([]);
+
+    const fetchBlogs = () => {
+        axios.get(`${process.env.REACT_APP_API}/blogs`)
+            .then(response => {
+                console.log(response);
+                setBlogs(response.data);
+            })
+            .catch(error => {
+                console.log(error);
+                alert('Cannot fetch blog posts');
+            });
+    };
+
+    useEffect(() => {
+        fetchBlogs();
+    }, []);
+
+    return (
+        <div className="container pb-5">
+            <Nav />
+            <br />
+            <h1>NCI SMART LOGIN</h1>
+            <hr/>
+            {
+                blogs.map((posts, i) => (
+                    <div className='row'key={posts._id} style={{borderBottom: '2px solid black'}}>
+                        <div className='col pt-3 pb-2'>
+                          <h2>{posts.title}</h2>  
+                          <p className='lead'>{posts.content.substring(0, 100)}</p>
+                          <p>Blog Author <span className='"badge badge-info"'>{posts.user}</span> Published on {''} <span className='"badge badge-info"'>{new Date (posts.createdAt).toLocaleString()}</span> </p>
+                        </div>
+                    </div>
+
+
+                ))
+
+
+            }
+
+        </div>
+    );
+};
 
 export default App;
+
